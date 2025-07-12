1008bf42c13749ac484da9266f3d9afb7dacc375
Thanutham-T <thanutham.tn@gmail.com>
2025-07-13 05:29:53 +0700

    Add booking router with CRUD endpoints for managing bookings and rescheduling

--- travelothai/routers/v1/__init__.py
+++ travelothai/routers/v1/__init__.py
@@ -3,7 +3,7 @@ from . import (
     province_router,
     hotel_router,
     ticket_router,
-    # booking_router,
+    booking_router,
     # user_router,
 )
 
@@ -11,5 +11,5 @@ router = APIRouter(prefix="/v1")
 router.include_router(province_router.router)
 router.include_router(hotel_router.router)
 router.include_router(ticket_router.router)
-# router.include_router(booking_router.router)
+router.include_router(booking_router.router)
 # router.include_router(user_router.router)
