7ecc6219e1da909f029d01b8a7d423077f2b5308
Thanutham-T <thanutham.tn@gmail.com>
2025-07-12 16:06:06 +0700

    Refactor Mock Province Service follow province interface.

--- travelothai/services/province_services/MockProvinceService.py
+++ travelothai/services/province_services/MockProvinceService.py
@@ -1,25 +1,26 @@
 import datetime
+from typing import List, Optional
 
 from .ProvinceServiceInterface import ProvinceServiceInterface
 from travelothai.schemas import province_schema
 
 
-mock_provinces: list[province_schema.Province] = [
+mock_provinces: List[province_schema.Province] = [
     province_schema.Province(id=1, name="กรุงเทพมหานคร", category_id=1, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now()),
     province_schema.Province(id=2, name="น่าน", category_id=2, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now()),
 ]
 mock_id = 3
 
 class MockProvinceService(ProvinceServiceInterface):
-    def list_provinces(self) -> list[province_schema.Province]:
+    def list_provinces(self) -> List[province_schema.Province]:
         return mock_provinces
-    
-    def get_province(self, province_id: int) -> province_schema.Province:
+
+    def get_province(self, province_id: int) -> Optional[province_schema.Province]:
         for province in mock_provinces:
             if province.id == province_id:
                 return province
-        raise ValueError("Province not found")
-    
+        return None
+
     def create_province(self, province: province_schema.ProvinceCreate) -> province_schema.Province:
         global mock_id
         new_province = province_schema.Province(
@@ -32,19 +33,17 @@ class MockProvinceService(ProvinceServiceInterface):
         mock_provinces.append(new_province)
         mock_id += 1
         return new_province
-    
-    def update_province(self, province_id: int, province: province_schema.ProvinceUpdate) -> province_schema.Province:
+
+    def update_province(self, province_id: int, province: province_schema.ProvinceUpdate) -> Optional[province_schema.Province]:
         for idx, existing_province in enumerate(mock_provinces):
             if existing_province.id == province_id:
                 updated_province = existing_province.model_copy(update=province.model_dump(exclude_unset=True))
                 updated_province.updated_at = datetime.datetime.now()
                 mock_provinces[idx] = updated_province
                 return updated_province
-        raise ValueError("Province not found")
-    
+        return None
+
     def delete_province(self, province_id: int) -> None:
         global mock_provinces
         mock_provinces = [province for province in mock_provinces if province.id != province_id]
-        if len(mock_provinces) == 0:
-            raise ValueError("Province not found")
         return None
