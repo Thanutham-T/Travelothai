eb3f45a694a9e431cd614750fa57f67dbeadc403
Thanutham-T <thanutham.tn@gmail.com>
2025-07-12 17:36:46 +0700

    add a relationship between a Hotel model and a Province model

--- travelothai/models/province_model.py
+++ travelothai/models/province_model.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
 
+from travelothai.schemas.hotel_schema import Hotel
+
 # ProvinceCategory model
 class ProvinceCategoryBase(SQLModel):
     name: str
@@ -10,6 +12,10 @@ class ProvinceCategory(ProvinceCategoryBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
+
+    # Relationship
+    provinces: List["Province"] = Relationship(back_populates="category")
+
 
 # Province model
 class ProvinceBase(SQLModel):
@@ -20,4 +26,7 @@ class Province(ProvinceBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
+
+    # Relationship
+    hotels: List["Hotel"] = Relationship(back_populates="province")
     category: Optional[ProvinceCategory] = Relationship(back_populates="provinces")
