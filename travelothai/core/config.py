16955c548ef089f3bf5a2abfeb94c64149c10715
Thanutham-T <thanutham.tn@gmail.com>
2025-07-13 06:14:43 +0700

    Refactor settings configuration to improve clarity and structure

--- travelothai/core/config.py
+++ travelothai/core/config.py
@@ -3,8 +3,12 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     USE_MOCK: bool = False
 
-    class Config:
-        env_file = ".env"
+    SECRET_KEY: str = "secret"
+    
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 5 * 60  # 5 minutes
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 7 * 24 * 60  # 7 days
+
+    model_config = {"env_file": ".env", "validate_assignment": True, "extra": "allow"}
 
 def get_settings() -> Settings:
     return Settings()
