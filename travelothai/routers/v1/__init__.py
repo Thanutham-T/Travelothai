80017ae3d3b93c21c65801ce3ae8454d2fd2db7f
Thanutham-T <thanutham.tn@gmail.com>
2025-07-13 03:40:37 +0700

    Add ticket router with CRUD endpoints for ticket types, usage rules, campaigns, and tickets

--- travelothai/routers/v1/__init__.py
+++ travelothai/routers/v1/__init__.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter
 from . import (
     province_router,
     hotel_router,
+    ticket_router,
     # booking_router,
-    # ticket_router,
     # user_router,
 )
 
 router = APIRouter(prefix="/v1")
 router.include_router(province_router.router)
 router.include_router(hotel_router.router)
+router.include_router(ticket_router.router)
 # router.include_router(booking_router.router)
-# router.include_router(ticket_router.router)
 # router.include_router(user_router.router)
